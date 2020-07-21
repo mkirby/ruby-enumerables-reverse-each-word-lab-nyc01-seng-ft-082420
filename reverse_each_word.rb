c97ee76aa7c0d4e15548af741a6099cace3a619e
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 17:40:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(sentence)
   split_words = sentence.split(" ")
   backwards_sentence_array = []
+  puts split_words
   
   split_words.each do |index|
     
-    split_word = split_words[index].split("")
-    puts "Split Word #{split_word}"
+    #split_word = split_words[index].split("")
+    #puts "Split Word #{split_word}"
     
   end
   
