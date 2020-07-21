fd104ab99aa2588ddf771dc816dc6cc0fce952bf
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 17:36:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(sentence)
-  split_sentence = sentence.split("")
-  p split_sentence
+  split_words = sentence.split(" ")
+  backwards_sentence_array = []
+  
+  
+  split_sentence.each do |index|
+    backwards_sentence_array.unshift(split_sentence[index])
+  end
+  puts backwards_sentence_array.to_s
 end
 
 reverse_each_word("Hello Cruel World")
